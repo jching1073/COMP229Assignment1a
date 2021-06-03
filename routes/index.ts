b095e5dc6b87abd1da17c61268e9f09d57b58030
Jerome Ching <nathan_9_9@hotmail.com>
2021-06-02 23:33:12 -0400

    Partial Pages

--- routes/index.ts
+++ routes/index.ts
@@ -4,30 +4,30 @@ export default router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'Home', page: 'home'});
 });
 /*Get Home Page*/
 router.get('/home', function (req, res, next)
 {
-  res.render('index', {title: "Home"});
+  res.render('index', {title: "Home", page: 'home'});
 });
 /*Get About Page*/
 router.get('/about', function (req, res, next)
 {
-  res.render('index', {title: "About"});
+  res.render('index', {title: "About Us", page: 'about'});
 });
 /*Get Project Page*/
 router.get('/projects', function (req, res, next)
 {
-  res.render('index', {title: "Projects"});
+  res.render('index', {title: "Our Projects", page: 'projects'});
 });
 /*Get Services Page*/
 router.get('/services', function (req, res, next)
 {
-  res.render('index', {title: "Services"});
+  res.render('index', {title: "Our Services", page: 'services'});
 });
 /*Get Contact Page*/
 router.get('/contact', function (req, res, next)
 {
-  res.render('index', {title: "Contact"});
+  res.render('index', {title: "Contact Us", page: 'contact'});
 });
