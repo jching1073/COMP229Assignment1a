c38a4dc7fc189038e75c271a4fbb588565830ca1
Jerome Ching <nathan_9_9@hotmail.com>
2021-06-02 22:32:43 -0400

    Added Navs with Routings

--- routes/index.ts
+++ routes/index.ts
@@ -4,5 +4,30 @@ export default router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home'});
+});
+/*Get Home Page*/
+router.get('/home', function (req, res, next)
+{
+  res.render('index', {title: "Home"});
+});
+/*Get About Page*/
+router.get('/about', function (req, res, next)
+{
+  res.render('index', {title: "About"});
+});
+/*Get Project Page*/
+router.get('/projects', function (req, res, next)
+{
+  res.render('index', {title: "Projects"});
+});
+/*Get Services Page*/
+router.get('/services', function (req, res, next)
+{
+  res.render('index', {title: "Services"});
+});
+/*Get Contact Page*/
+router.get('/contact', function (req, res, next)
+{
+  res.render('index', {title: "Contact"});
 });
